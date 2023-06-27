46b5cec083d8fd618e1a9d96bca35a2020a3a645
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-06-27 16:13:57 +0200

    setup leaderboard

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+import { renderLeaderboard, handleFormSubmit } from './modules/leaderboard.js';
+
+// Attach form submit event listener
+document.getElementById('scoreForm').addEventListener('submit', handleFormSubmit);
+
+// Initial rendering of the leaderboard
+renderLeaderboard();
